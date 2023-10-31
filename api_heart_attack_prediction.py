0b3142c04d90a727107bb313038004162cb7a1de
Etsuko2003 <nestoraraba@gmail.com>
2023-10-31 21:37:18 +0100

    new changes

--- api_heart_attack_prediction.py
+++ api_heart_attack_prediction.py
@@ -3,27 +3,28 @@ import uvicorn
 from fastapi import FastAPI
 import pandas as pd
 from prometheus_client import Counter, make_asgi_app
+from starlette.responses import JSONResponse
 
-survived_counter = Counter("survived", "Counter for survived")
-not_survived_counter = Counter("not_survived", "Counter for not survived")
+# survived_counter = Counter("survived", "Counter for survived")
+# not_survived_counter = Counter("not_survived", "Counter for not survived")
 
 app = FastAPI()
-metrics_app = make_asgi_app()
-app.mount("/metrics", metrics_app)
+# metrics_app = make_asgi_app()
+# app.mount("/metrics", metrics_app)
 
 
-@app.get("/titanic")
-def prediction_api(pclass: int, sex: int, age: int):
-    titanic_model = joblib.load("./titanic_model.joblib")
-    x = [pclass, sex, age]
-    prediction = titanic_model.predict(pd.DataFrame(x).transpose())
-    survived = int(prediction) == 1
-    if survived:
-        survived_counter.inc()
-    else:
-        not_survived_counter.inc()
-    return survived
+@app.get("/heart_attack")
+def prediction_api(time: int, ejection_fraction: float, serum_creatinine: float):
+    heart_attack_model = joblib.load("./heart_attack_prediction_model.joblib")
+    x = [time, ejection_fraction, serum_creatinine]
+    prediction = heart_attack_model.predict(pd.DataFrame(x).transpose())
+
+#    if survived:
+#        survived_counter.inc()
+#    else:
+#       not_survived_counter.inc()
+    return JSONResponse(content={"prediction": prediction.tolist()})
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="127.0.0.1", port=5000)
