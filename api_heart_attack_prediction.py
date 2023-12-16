0ff8824e8c8cd5ddc704ccdbbd242a73250c5405
Christian Thomas  DeBADOLO <98065801+debad1@users.noreply.github.com>
2023-12-16 22:04:25 +0100

    update

--- api_heart_attack_prediction.py
+++ api_heart_attack_prediction.py
@@ -3,31 +3,36 @@ import uvicorn
 from fastapi import FastAPI
 import pandas as pd
 from prometheus_client import Counter, make_asgi_app
-from starlette.responses import JSONResponse
 
-survived_counter = Counter("survived", "Counter for survived")
-not_survived_counter = Counter("not_survived", "Counter for not survived")
+# Initialiser le modèle
+heart_attack_model = joblib.load("./heart_attack_prediction_model.joblib")
 
+# Définition des compteurs Prometheus
+survived_counter = Counter("survived", "Counter for survived predictions")
+not_survived_counter = Counter("not_survived", "Counter for not survived predictions")
 
 app = FastAPI()
+
+# Monter l'application de métriques Prometheus
 metrics_app = make_asgi_app()
 app.mount("/metrics", metrics_app)
 
-
 @app.get("/heart_attack")
 def prediction_api(time: int, ejection_fraction: float, serum_creatinine: float):
-    heart_attack_model = joblib.load("./heart_attack_prediction_model.joblib")
-    x = pd.DataFrame([time, ejection_fraction, serum_creatinine]).transpose()
-    x.columns = ['time', 'ejection_fraction', 'serum_creatinine']
-    prediction = heart_attack_model.predict(x)
-    survived = int(prediction) == 1
+    # Préparer les données pour la prédiction
+    x = pd.DataFrame([[time, ejection_fraction, serum_creatinine]], columns=['time', 'ejection_fraction', 'serum_creatinine'])
 
+    # Faire la prédiction
+    prediction = heart_attack_model.predict(x)
+    survived = int(prediction[0]) == 1
+
+    # Incrémenter le compteur approprié
     if survived:
         survived_counter.inc()
     else:
         not_survived_counter.inc()
-    return survived
 
+    return {"survived": survived}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=5000)
