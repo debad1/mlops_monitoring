280e24bb69353f1763153dcbcd34d239591e0cad
Christian Thomas  DeBADOLO <98065801+debad1@users.noreply.github.com>
2023-12-16 23:04:26 +0100

    Update api_heart_attack_prediction.py

--- api_heart_attack_prediction.py
+++ api_heart_attack_prediction.py
@@ -9,7 +9,8 @@ heart_attack_model = joblib.load("./heart_attack_prediction_model.joblib")
 
 # Définition des compteurs Prometheus
 survived_counter = Counter("survived", "Counter for survived predictions")
-not_survived_counter = Counter("not_survived", "Counter for not survived predictions")
+not_survived_counter = Counter(
+    "not_survived", "Counter for not survived predictions")
 
 app = FastAPI()
 
@@ -17,10 +18,12 @@ app = FastAPI()
 metrics_app = make_asgi_app()
 app.mount("/metrics", metrics_app)
 
+
 @app.get("/heart_attack")
 def prediction_api(time: int, ejection_fraction: float, serum_creatinine: float):
     # Préparer les données pour la prédiction
-    x = pd.DataFrame([[time, ejection_fraction, serum_creatinine]], columns=['time', 'ejection_fraction', 'serum_creatinine'])
+    x = pd.DataFrame([[time, ejection_fraction, serum_creatinine]], columns=[
+                     'time', 'ejection_fraction', 'serum_creatinine'])
 
     # Faire la prédiction
     prediction = heart_attack_model.predict(x)
@@ -33,6 +36,7 @@ def prediction_api(time: int, ejection_fraction: float, serum_creatinine: float)
         not_survived_counter.inc()
 
     return {"survived": survived}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=5000)
